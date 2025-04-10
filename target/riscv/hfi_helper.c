694a70fed448f7ba529247a4382dcc66c84b36d3
Dinesh Keserla <dineshkeserla@utexas.edu>
2025-04-10 18:21:15 -0500

    Basic Instructions Complete and Tested

--- target/riscv/hfi_helper.c
+++ target/riscv/hfi_helper.c
@@ -4,15 +4,11 @@
 
 void helper_hfi_enter(CPURISCVState *env, uint64_t exit_handler)
 {
-
-    env->gpr[17] = 0xDEADBEEF;  
     /* Set HFI sandbox active: status = 1 and record the 64-bit exit handler */
     env->hfi_status = 1;
     env->hfi_exit_pc = exit_handler;
 
-    qemu_log_mask(LOG_UNIMP, "HFI: Enter sandbox mode\n");
-    // fprintf(stderr, "HFI ENTER: status=1, exit_handler=0x%016llx\n", (unsigned long long)exit_handler);
-
+    qemu_log_mask(LOG_UNIMP, "HFI: Enter sandbox mode, exit_handler=0x%016" PRIx64 "\n", exit_handler);
 }
 
 void helper_hfi_exit(CPURISCVState *env)
@@ -21,4 +17,5 @@ void helper_hfi_exit(CPURISCVState *env)
     env->hfi_status = 0;
     env->hfi_exit_pc = 0;
     qemu_log_mask(LOG_UNIMP, "HFI: Exited sandbox mode\n");
+    exit(0);
 }
